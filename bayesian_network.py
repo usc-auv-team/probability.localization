b1b9b78ec3a6827aac8f82a32b44a5f6c37227fa
aayushshah96 <aayush1996@hotmail.com>
2018-10-24 19:32:44 -0700

    Updated Bayesian network code

--- bayesian_network.py
+++ bayesian_network.py
@@ -1,36 +1,65 @@
-from pgmpy.models import BayesianModel
-from pgmpy.factors.discrete import TabularCPD
-from time import clock
-import pandas as pd
-start = clock()
+'''This is the earthquake example from 2.5.1 in BAI'''
+from bayesian.bbn import build_bbn
 
-G = BayesianModel()
 
-G.add_edge('Burglary', 'Alarm')
-G.add_edge('Earthquake', 'Alarm')
-#G.add_edge('Alarm', 'JohnCalls')
-#G.add_edge('Alarm', 'MaryCalls')
-#print(len(G))
+def f_burglary(burglary):
+    if burglary is True:
+        return 0.01
+    return 0.99
 
-cpd_burglary = TabularCPD('Burglary', 2, [[0.999],[0.001]])
-cpd_earthquake = TabularCPD('Earthquake', 2, [[0.998],[0.002]])
-cpd_alarm = TabularCPD('Alarm', 2, [[0.999, 0.71, 0.06, 0.05],[0.001, 0.29, 0.94, 0.95]], evidence = ['Burglary','Earthquake'], evidence_card=[2,2])
-cpd_JC = TabularCPD('JohnCalls', 2, [[0.95, 0.10],[0.05, 0.90]], evidence = ['Alarm'], evidence_card=[2])
-cpd_MC = TabularCPD('MaryCalls', 2, [[0.99, 0.30],[0.01, 0.70]], evidence = ['Alarm'], evidence_card=[2])
-#print(cpd_MC)
-#print(G.check_model())
-#G.add_cpds(cpd_burglary, cpd_earthquake, cpd_alarm, cpd_JC, cpd_MC)
-G.add_cpds(cpd_burglary, cpd_earthquake, cpd_alarm)
-#print(G.check_model())
 
-d = {'Burglary': [1, 1], 'Alarm': [1, 1]}
-df = pd.DataFrame(data = d)
+def f_earthquake(earthquake):
+    if earthquake is True:
+        return 0.02
+    return 0.98
 
-#values = pd.DataFrame(columns = 'Burglary': [0, 1], 'Alarm': [1, 1])
 
-#G.fit(d)
+def f_alarm(burglary, earthquake, alarm):
+    table = dict()
+    table['ttt'] = 0.95
+    table['ttf'] = 0.05
+    table['tft'] = 0.94
+    table['tff'] = 0.06
+    table['ftt'] = 0.29
+    table['ftf'] = 0.71
+    table['fft'] = 0.001
+    table['fff'] = 0.999
+    key = ''
+    key = key + 't' if burglary else key + 'f'
+    key = key + 't' if earthquake else key + 'f'
+    key = key + 't' if alarm else key + 'f'
+    return table[key]
 
-solution = G.predict(df)
-print(solution)
 
-#print(clock()-start)
+def f_johncalls(alarm, johncalls):
+    table = dict()
+    table['tt'] = 0.9
+    table['tf'] = 0.1
+    table['ft'] = 0.05
+    table['ff'] = 0.95
+    key = ''
+    key = key + 't' if alarm else key + 'f'
+    key = key + 't' if johncalls else key + 'f'
+    return table[key]
+
+
+def f_marycalls(alarm, marycalls):
+    table = dict()
+    table['tt'] = 0.7
+    table['tf'] = 0.3
+    table['ft'] = 0.01
+    table['ff'] = 0.99
+    key = ''
+    key = key + 't' if alarm else key + 'f'
+    key = key + 't' if marycalls else key + 'f'
+    return table[key]
+
+
+if __name__ == '__main__':
+    g = build_bbn(
+        f_burglary,
+        f_earthquake,
+        f_alarm,
+        f_johncalls,
+        f_marycalls)
+    g.q()
